85f370f38cc91a65fb4983aa555866247423adb4
RXT-BRAMD <157570608+RXT-RS@users.noreply.github.com>
2024-03-11 17:27:31 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,55 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
-keys = set()
+all_key_file = "all_keys.json"
+try:
+    all_keys = open(all_key_file, "r").read()
+    key_data = json.loads(all_keys)
+except:
+    key_data = {}
 
-@app.route('/approve', methods=['GET'])
-def approve_key():
+@app.route("/keys")
+def showKeys():
+    keys = ""
+    for k, v in key_data.items():
+        if v == True:
+            keys += k + "<br>"
+    return f"<p>{keys}</p>"
+
+@app.route('/add_key')
+def add_key():
     key = request.args.get('key')
-    if key:
-        keys.add(key)
-        return jsonify({'Status': 'Key approved successfully'})
+    password = request.args.get('password')
+    if password == "adminPassword123":
+        if key is not None:
+            key_data[key] = True
+            open(all_key_file, "w").write(json.dumps(key_data))
+            return json.dumps({'status': 'success', 'message': 'Key added successfully!'}), 200
+        else:
+            return json.dumps({'status': 'fail', 'message': 'Invalid request'}), 404
     else:
-        return jsonify({'Status': 'Key not provided'})
+        return json.dumps({'status': 'fail', 'message': 'Action Not allowed for non admin users'}), 404
 
-@app.route('/approve_key_put', methods=['GET'])
-def approve_key_put():
+@app.route('/remove_key')
+def remove_key():
     key = request.args.get('key')
-    if key:
-        keys.add(key)
-        return jsonify({'Status': 'Key added successfully'})
+    password = request.args.get('password')
+    if password == "adminPassword123":
+        if key is not None and key in key_data:
+            del key_data[key]
+            open(all_key_file, "w").write(json.dumps(key_data))
+            return json.dumps({'status': 'success', 'message': 'Key removed successfully!'}), 200
+        else:
+            return json.dumps({'status': 'fail', 'message': 'Key not found'}), 404
     else:
-        return jsonify({'Status': 'Key not provided'})
+        return json.dumps({'status': 'fail', 'message': 'Action Not allowed for non admin users'}), 404
 
-@app.route('/approve_key_rmv', methods=['GET'])
-def approve_key_rmv():
-    key = request.args.get('key')
-    if key in keys:
-        keys.remove(key)
-        return jsonify({'Status': 'Key removed successfully'})
-    else:
-        return jsonify({'Status': 'Key not found'})
-
-@app.route('/check_key', methods=['GET'])
+@app.route('/check_key')
 def check_key():
     key = request.args.get('key')
-    if key in keys:
-        return jsonify({'Status': 'Key exists'})
+    if key is not None and key in key_data:
+        return json.dumps({'status': 'success', 'message': 'Key found'}), 200
     else:
-        return jsonify({'Status': 'Key not found'})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        return json.dumps({'status': 'fail', 'message': 'Key not found'}), 404
